e2ea88c96512145835b978892fe2d613e91d9566
Andreas Fritz <eisinator@arcor.de>
2012-06-02 17:51:29 +0200

    Google Login Klasse

---
+++ Google Sync/src/GoogleLogin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+#region Google API
+using Google.GData.Client;
+#endregion
+
+namespace Google_Sync.src
+{
+    class GoogleLogin
+    {
+        private string authToken;
+        private string username;
+
+        private Service service;
+
+        public GoogleLogin(Service serviceToUse)
+        {
+            this.service = serviceToUse;
+        }
+
+        public GoogleLogin(Service serviceToUse, string username)
+        {
+            this.service = serviceToUse;
+            this.username = username;
+        }
+
+        public string AuthenticationToken
+        {
+            get
+            {
+                return this.authToken;
+            }
+        }
+    }
+}
